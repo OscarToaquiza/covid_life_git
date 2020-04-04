275801b10ad1858fe1b33e44b43380a4b41a9132
OscarToaquiza <oscartoaquiza388@gmail.com>
2020-04-04 17:49:54 -0500

    Cambie la forma de obtener la fft del vetor R, y se envia a David para su revision

--- app/src/main/java/com/covid/life/manage/camera/pre_camera.java
+++ app/src/main/java/com/covid/life/manage/camera/pre_camera.java
@@ -66,10 +66,15 @@ public class pre_camera extends AppCompatActivity {
 
         if(puntos.size() > 0){
             graficaUno();
-            Log.d("TamanioVecotr",""+puntos.size());
-            Log.d("Tiempo",""+seconds);
-            Log.d("Tiempo",""+(puntos.size()/seconds));
+            //Log.d("Tamaño del Vector",""+puntos.size());
+            //Log.d("Tiempo Segundos",""+seconds);
+            //Log.d("Frame por Segundo",""+(puntos.size()/seconds));
 
+            // Imprimir el vector R.
+
+            for( int w = 0; w < puntos.size(); w++){
+                Log.d("Vector R"+w,""+puntos.get(w));
+            }
             graficaDos(puntos,seconds);
 
         }
@@ -101,15 +106,221 @@ public class pre_camera extends AppCompatActivity {
             vector[i] = valorY;
             i++;
         }
-
         long frmSegundo = (puntosVectorR.size()/tiempo);
         Log.d("VectorR FOURIER",""+vector.length);
         Log.d("Tiempo FOURIER",""+tiempo);
         Log.d("frameSegundo FOURIER",""+frmSegundo);
 
-        DoubleFFT_1D fft = new DoubleFFT_1D( vector.length );
-        //fft.complexForward(vector);
-        fft.realForward(vector);
+        DoubleFFT_1D fft = new DoubleFFT_1D(vector.length);
+        Log.d("DataNueva","-------------------------------------------");
+        //fft.realForward(vector);
+        //double[] a = new double[ vector.length * 2];
+        //System.arraycopy(vector, 0, a, 0, vector.length);
+        fft.realForward( vector );
+
+        double[] real = new double[ vector.length/2 ];
+        double[] imag = new double[ vector.length/2 ];
+
+        for(int g = 0; g < vector.length / 2; ++g) {
+            double re  = vector[2*g];
+            double im  = vector[2*g+1];
+            Log.d("Real",""+re);
+            Log.d("Img",""+im);
+        }
+
+        Log.d("DataNueva","-------------------------------------------");
+
+        //for( int z = 0; z < vector.length;z++ ){
+         //   System.out.println(vector[z]);
+        //}
+
+/*
+        if(vector.length >= 64 && vector.length < 128){
+            int z = 0;
+            vectorFurier = new double[ 64 ];
+            for( z = 0; z < 65;z++ ){
+                vectorFurier[z] = vector[z];
+            }
+            Log.d("Tamaño Vector FOURIER", ""+vectorFurier.length );
+            fft  = new DoubleFFT_1D( vectorFurier.length );
+            fft.complexForward(vectorFurier);
+            z = 0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteCompleja",""+ vectorFurier[z]);
+            }
+            fft.realForward(vectorFurier);
+            z=0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteEntera",""+ vectorFurier[z]);
+            }
+        }
+
+
+
+
+
+
+        if(vector.length >= 128 && vector.length < 256){
+            int z = 0;
+            vectorFurier = new double[ 128 ];
+            for( z = 0; z < 129;z++ ){
+                vectorFurier[z] = vector[z];
+            }
+            Log.d("Tamaño Vector FOURIER", ""+vectorFurier.length );
+            fft  = new DoubleFFT_1D( vectorFurier.length );
+            fft.complexForward(vectorFurier);
+            z = 0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteCompleja",""+ vectorFurier[z]);
+            }
+            fft.realForward(vectorFurier);
+            z=0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteEntera",""+ vectorFurier[z]);
+            }
+        }
+
+
+
+
+        if(vector.length >= 256 && vector.length < 512){
+            int z = 0;
+            vectorFurier = new double[ 256 ];
+            for( z = 0; z < 256;z++ ){
+                vectorFurier[z] = vector[z];
+            }
+            Log.d("Tamaño Vector FOURIER", ""+vectorFurier.length );
+            fft  = new DoubleFFT_1D( vectorFurier.length);
+            fft.realForward(vectorFurier);
+            z = 0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("VF",""+ vectorFurier[z]);
+            }
+
+            fft.realForward(vectorFurier);
+            z=0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteEntera",""+ vectorFurier[z]);
+            }
+        }
+
+
+
+
+
+
+        if(vector.length >= 512 && vector.length < 1024){
+            int z = 0;
+            vectorFurier = new double[ 512 ];
+            for( z = 0; z < 513;z++ ){
+                vectorFurier[z] = vector[z];
+            }
+            Log.d("Tamaño Vector FOURIER", ""+vectorFurier.length );
+            fft  = new DoubleFFT_1D( vectorFurier.length );
+            fft.complexForward(vectorFurier);
+            z = 0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteCompleja",""+ vectorFurier[z]);
+            }
+            fft.realForward(vectorFurier);
+            z=0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteEntera",""+ vectorFurier[z]);
+            }
+        }
+
+
+
+
+
+
+
+        if(vector.length >= 1024 && vector.length < 2048){
+            int z = 0;
+            vectorFurier = new double[ 1024 ];
+            for( z = 0; z < 1025;z++ ){
+                vectorFurier[z] = vector[z];
+            }
+            Log.d("Tamaño Vector FOURIER", ""+vectorFurier.length );
+            fft  = new DoubleFFT_1D( vectorFurier.length );
+            fft.complexForward(vectorFurier);
+            z = 0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteCompleja",""+ vectorFurier[z]);
+            }
+            fft.realForward(vectorFurier);
+            z=0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteEntera",""+ vectorFurier[z]);
+            }
+        }
+
+
+
+
+        if(vector.length >= 2048 && vector.length < 4096){
+            int z = 0;
+            vectorFurier = new double[ 2048 ];
+            for( z = 0; z < 2049;z++ ){
+                vectorFurier[z] = vector[z];
+            }
+            Log.d("Tamaño Vector FOURIER", ""+vectorFurier.length );
+            fft  = new DoubleFFT_1D( vectorFurier.length );
+            fft.complexForward(vectorFurier);
+            z = 0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteCompleja",""+ vectorFurier[z]);
+            }
+            fft.realForward(vectorFurier);
+            z=0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteEntera",""+ vectorFurier[z]);
+            }
+        }
+
+        if(vector.length >= 4096 && vector.length < 8192){
+            int z = 0;
+            vectorFurier = new double[ 4096 ];
+            for( z = 0; z < 4097;z++ ){
+                vectorFurier[z] = vector[z];
+            }
+            Log.d("Tamaño Vector FOURIER", ""+vectorFurier.length );
+            fft  = new DoubleFFT_1D( vectorFurier.length );
+            fft.complexForward(vectorFurier);
+            z = 0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteCompleja",""+ vectorFurier[z]);
+            }
+            fft.realForward(vectorFurier);
+            z=0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteEntera",""+ vectorFurier[z]);
+            }
+        }
+
+        if(vector.length >= 8192 && vector.length < 16384){
+
+            int z = 0;
+            vectorFurier = new double[ 8192 ];
+            for( z = 0; z < 8193;z++ ){
+                vectorFurier[z] = vector[z];
+            }
+            Log.d("Tamaño Vector FOURIER", ""+vectorFurier.length );
+            fft  = new DoubleFFT_1D( vectorFurier.length );
+            fft.complexForward(vectorFurier);
+            z = 0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteCompleja",""+ vectorFurier[z]);
+            }
+            fft.realForward(vectorFurier);
+            z=0;
+            for( z = 0; z < vectorFurier.length;z++ ){
+                Log.d("ParteEntera",""+ vectorFurier[z]);
+            }
+        }
+
+*/
+
         /*
         * Fs = 100 100 muestras por cada 10 segundos
         * T = 1/Fs
@@ -156,8 +367,9 @@ public class pre_camera extends AppCompatActivity {
         double[] vectorPromedio = new double[ ( (int) frmSegundo/2) ];
         for( i = 0; i < vectorPromedio.length; i++){
             vectorPromedio[i] = vector[i+1];
-            promedioGeneral = promedioGeneral + vector[i+1];
+            //promedioGeneral = promedioGeneral + vector[i+1];
         }
+
 
         Log.d("ArrayProm",Arrays.toString(vectorPromedio));
         promedioGeneral = promedioGeneral/( (int) frmSegundo/2);
